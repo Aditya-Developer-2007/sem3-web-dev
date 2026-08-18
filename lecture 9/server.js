032ca2e1a3e2e5aef34da24e2bb607746130e8c8
Aditya-Developer-2007 <aaditya.saini2602@gmail.com>
2026-08-18 11:12:55 +0530

    Add routes and nodemon in lecture 9

--- lecture 9/server.js
+++ lecture 9/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
-
 const app = express();
 
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.get("/about", (req, res) => {
+    res.send("About page");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Contact page");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
